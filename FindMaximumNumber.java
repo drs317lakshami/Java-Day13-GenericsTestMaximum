b9e694bc8eb7ae8a7ea676a94a525f7526be2338
drs317lakshami <devrajkumar317@gmail.com>
2022-10-02 00:47:36 +0530

    extended max method to sort based on option

--- FindMaximumNumber.java
+++ FindMaximumNumber.java
@@ -2,44 +2,70 @@ package com.generics;
 
 import java.util.ArrayList;
 import java.util.Collections;
+
 public class FindMaximumNumber<E extends Comparable> {
 
-	  private E a, b, c;
+  private E a, b, c;
 
-	  public FindMaximumNumber(E a, E b, E c) {
-	    this.a = a;
-	    this.b = b;
-	    this.c = c;
+  public FindMaximumNumber(E a, E b, E c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
 
-	    this.testMaximum(); 
-	  }
-	  public E testMaximum() {
-	    E max = FindMaximumNumber.testMaximum(a, b, c);
-	    System.out.println("max is " + max);
-	    return max;
-	  }
+  public E testMaximum(String option) {
+    E max = FindMaximumNumber.testMaximum(a, b, c, option);
+    return max;
+  }
 
-	  public static void main(String[] args) {
-	   
-	    FindMaximumNumber<Integer> intIns = new FindMaximumNumber<>(10, 2, 3); 
+  public static void main(String[] args) {
+    //Test case 1: max int at 1st position
+    FindMaximumNumber<Integer> intIns = new FindMaximumNumber<>(10, 2, 3);
+    System.out.println("Max= " + intIns.testMaximum("desc") + "\n"); 
 
-	    new FindMaximumNumber<Float>(10f, 20f, 3f);
+    //Test case 2: max float number at 2nd position
+    FindMaximumNumber<Float> floatIns = new FindMaximumNumber<>(10f, 20f, 3f);
+    System.out.println("Max= " + floatIns.testMaximum("asc") + "\n");
 
-	    new FindMaximumNumber<String>("Chase", "Patric", "Ryan");
+    //Test case 3: max string at 3rd position
+    FindMaximumNumber<String> stringIns = new FindMaximumNumber<>("Patric", "Chase", "Ryan");
+    System.out.println("Max= " + stringIns.testMaximum("desc") + "\n");
 
-	  }
+  }
 
-	  @SuppressWarnings({"unchecked", "rawtypes"})
-	  public static <E extends Comparable> E testMaximum(E a, E b, E c) {
-	    E max; 
-	    if (a.compareTo(b) > 0) {
-	      if (a.compareTo(c) > 0) max = a;
-	      else max = c;
+  @SuppressWarnings({"unchecked", "rawtypes"})
+  public static <E extends Comparable> E testMaximum(E a, E b, E c, String option) {
+    E max; 
+    ArrayList<E> arrList = new ArrayList<>(); 
+    arrList.add(a);
+    arrList.add(b);
+    arrList.add(c);
 
-	    } else if (b.compareTo(c) > 0) max = b;
-	    else max = c;
+    if (a.compareTo(b) > 0) {
+      if (a.compareTo(c) > 0) max = a;
+      else max = c;
 
-	    return max;
-	  }
+    } else if (b.compareTo(c) > 0) max = b;
+    else max = c;
 
-	}
+    switch (option) {
+    case "asc":
+      System.out.println("before sorting: " + arrList);
+      Collections.sort(arrList);
+      System.out.println("after sorting (asc): " + arrList);
+      break;
+
+    case "desc":
+      System.out.println("before sorting: " + arrList);
+      Collections.sort(arrList, Collections.reverseOrder());
+      System.out.println("after sorting (desc): " + arrList);
+      break;
+
+    default:
+      System.out.println("that option doesnt exist");
+    }
+
+    return max;
+  }
+
+}
